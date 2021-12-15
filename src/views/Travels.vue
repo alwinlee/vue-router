97f5247fe99b48322e6e1a0398b098ca0c2f10b6
Alwin <alwinlee0915@gmail.com>
2021-12-15 15:40:46 +0800

    add travel page

---
+++ src/views/Travels.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="travels">
+    <p>Have fun!</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Travels',
+  components: {
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
